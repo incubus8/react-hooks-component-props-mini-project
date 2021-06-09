fdb25de190e1fd6e95b9bc913f33f465ea4d22e4
Terry <incubus8@gmail.com>
2021-06-08 22:41:32 -0400

    finished, but no bonus

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import Article from "./Article"
+
+function ArticleList({posts}) {
+      
+    const mainArr = posts.map((post) => {    
+    return (
+        <Article
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+            minutes={post.minutes}
+        />    
+        )    
+    })
+    return <main>{mainArr}</main>
+}
+
+export default ArticleList
